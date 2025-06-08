015384b86fd7f0292ff7586bccd0a0460cd6aac3
codesigmax <160916892+codesigmax@users.noreply.github.com>
2025-06-09 07:21:29 +0800

    feat: 新增数据合理性校验，用户不能进行添加自己为联系人的请求

--- cosmos-im-server/src/main/java/com/qfleaf/cosmosimserver/contact/domain/aggregates/ContactAggregate.java
+++ cosmos-im-server/src/main/java/com/qfleaf/cosmosimserver/contact/domain/aggregates/ContactAggregate.java
@@ -4,10 +4,12 @@ import com.qfleaf.cosmosimserver.contact.domain.entities.ContactEntity;
 import com.qfleaf.cosmosimserver.core.domain.DomainEvent;
 import com.qfleaf.cosmosimserver.core.domain.base.BaseAggregateRoot;
 import com.qfleaf.cosmosimserver.core.domain.enums.ContactRelation;
+import com.qfleaf.cosmosimserver.shared.exception.InvalidArgsException;
 import lombok.Getter;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 public class ContactAggregate extends BaseAggregateRoot {
@@ -24,6 +26,9 @@ public class ContactAggregate extends BaseAggregateRoot {
             String remarkName,
             Long groupId
     ) {
+        if (Objects.equals(ownerId, contactId)) {
+            throw new InvalidArgsException("你不能添加自己为联系人");
+        }
         ContactAggregate contact = new ContactAggregate();
         contact.from = ContactEntity.builder()
                 .ownerId(ownerId)
