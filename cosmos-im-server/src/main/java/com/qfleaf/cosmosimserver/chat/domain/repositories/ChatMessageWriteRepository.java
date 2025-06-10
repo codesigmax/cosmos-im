169013dcbe44a9a8f6709ab1616111b41cc9e584
codesigmax <160916892+codesigmax@users.noreply.github.com>
2025-06-10 16:15:52 +0800

    fix: 持久化消息时回显id

--- cosmos-im-server/src/main/java/com/qfleaf/cosmosimserver/chat/domain/repositories/ChatMessageWriteRepository.java
+++ cosmos-im-server/src/main/java/com/qfleaf/cosmosimserver/chat/domain/repositories/ChatMessageWriteRepository.java
@@ -3,6 +3,7 @@ package com.qfleaf.cosmosimserver.chat.domain.repositories;
 import com.qfleaf.cosmosimserver.chat.domain.entites.ChatMessageEntity;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 
 @Mapper
 public interface ChatMessageWriteRepository {
@@ -10,5 +11,6 @@ public interface ChatMessageWriteRepository {
             insert into chat_messages(sender_id, receiver_id, content)
             values (#{senderId}, #{receiverId}, #{content})
             """)
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
     int insert(ChatMessageEntity chatMessageEntity);
 }
