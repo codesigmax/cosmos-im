98ac02654a6717fa65e96f74d7a4d99d679c71ba
codesigmax <160916892+codesigmax@users.noreply.github.com>
2025-06-10 18:20:14 +0800

    optimize: 代码格式化

--- cosmos-im-server/src/main/java/com/qfleaf/cosmosimserver/core/config/SpringDocConfig.java
+++ cosmos-im-server/src/main/java/com/qfleaf/cosmosimserver/core/config/SpringDocConfig.java
@@ -13,20 +13,21 @@ public class SpringDocConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("Cosmos-IM API 文档")  // 文档标题
-                        .version("alpha v0.1")               // 版本号
-                        .description("Cosmos-IM 是基于SpringBoo3的即时通讯系统，当前为alpha版本，功能完善中。") // 描述
-                        .contact(
-                                new Contact()        // 联系人信息
-                                        .name("搴芳")
-                                        .url("https://codesigmax.github.io")
-                        )
-                        .license(
-                                new License()        // 开源协议
-                                        .name("Apache 2.0")
-                                        .url("https://www.apache.org/licenses/LICENSE-2.0.html")
-                        )
+                .info(
+                        new Info()
+                                .title("Cosmos-IM API 文档")  // 文档标题
+                                .version("alpha v0.1")               // 版本号
+                                .description("Cosmos-IM 是基于SpringBoo3的即时通讯系统，当前为alpha版本，功能完善中。") // 描述
+                                .contact(
+                                        new Contact()        // 联系人信息
+                                                .name("搴芳")
+                                                .url("https://codesigmax.github.io")
+                                )
+                                .license(
+                                        new License()        // 开源协议
+                                                .name("Apache 2.0")
+                                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")
+                                )
                 );
     }
 }
